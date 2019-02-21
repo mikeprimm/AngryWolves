e7d56e9b968449fd1749fbebd229916686aadeac
Jeremy Denslinger <AeSix@users.noreply.github.com>
2019-02-21 08:12:22 -0500

    FOREST_HILLS -> WOODED_HILLS
    Let's see if it compiles now.

--- src/main/java/com/mikeprimm/bukkit/AngryWolves/AngryWolvesEntityListener.java
+++ src/main/java/com/mikeprimm/bukkit/AngryWolves/AngryWolvesEntityListener.java
@@ -128,7 +128,7 @@ public class AngryWolvesEntityListener implements Listener {
                 if(plugin.verbose) AngryWolves.log.info("biome=" + bio + ", ignore=" + ignore_terrain + ", angry=" + angry + ", hellhound=" + do_hellhound);
         		/* If valid biome for wolf (or hellhound) */
         		if(ignore_terrain || (bio == Biome.FOREST) || (bio == Biome.TAIGA) ||
-        				(bio == Biome.NETHER) || (bio == Biome.FOREST_HILLS) || (bio == Biome.TAIGA_HILLS)) {
+        				(bio == Biome.NETHER) || (bio == Biome.WOODED_HILLS) || (bio == Biome.TAIGA_HILLS)) {
     				/* If hellhound in hell, we're good */
     				if(bio.equals(Biome.NETHER)) {
     				    if(!do_hellhound)   /* Only angry hellhounds in nether */
